c0c5b27b9dc83ecaf6c36bfec33dd2bcf9994d33
Barney Powell <9741234+BarneyPowell@users.noreply.github.com>
2020-08-12 23:37:58 +0100

    Add simple background image section component (#1)
    Co-authored-by: Barney Powell <barneyp@gmail.com>

---
+++ components/_molecules/Layout/Home.js
@@ -0,0 +1,19 @@
+
+export default function Home({ children }) {
+    // max-w-screen-sm px-4 py-12 mx-auto antialiased font-body
+    return (
+        <div className="">
+        <header>
+
+        </header>
+        <main>
+            {children}
+        </main>
+        <footer className="text-lg font-light">
+          © {new Date().getFullYear()}, Built with{" "}
+          <a href="https://nextjs.org/">Next.js</a>
+          &#128293;
+        </footer>
+      </div>
+    )
+}
