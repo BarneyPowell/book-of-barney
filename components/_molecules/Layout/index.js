52ef529d0d34b325d671109f12b6039967155fa6
Barney Powell <barneyp@gmail.com>
2020-08-19 17:01:00 +0100

    Fix missing imports in layout

--- components/_molecules/Layout/index.js
+++ components/_molecules/Layout/index.js
@@ -1,5 +1,13 @@
+import Link from "next/link";
+import { useRouter } from "next/router";
+import Menu from "@/molecules/Menu";
 import Home from './Home';
 import Post from './Post';
+import Header from '@/molecules/Header';
+import Main from '@/molecules/Main';
+
+import Footer from '@/molecules/Footer';
+import React, { useState } from 'react';
 
 const TemplateMap = {
   'home': Home,
@@ -12,7 +20,6 @@ const getTemplate = (pathname) => {
     ? Home
     : TemplateMap[pathname.split('/')[1].toLowerCase()];
 }
-
 
 export default function Layout({ children }) {
   const { pathname } = useRouter();
