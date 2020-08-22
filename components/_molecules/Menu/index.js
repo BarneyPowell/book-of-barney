f01509c8848237ae492a8a2a4ab845063f5f1bb1
Barney Powell <barneyp@gmail.com>
2020-08-22 16:35:40 +0100

    Add social media icons to main menu

--- components/_molecules/Menu/index.js
+++ components/_molecules/Menu/index.js
@@ -1,4 +1,8 @@
 import PrimaryNavigation from '@/molecules/PrimaryNavigation';
+import TwitterSvg from '@/atoms/SvgIcon/TwitterSvg';
+import ImdbSvg from '@/atoms/SvgIcon/ImdbSvg';
+import InstagramSvg from '@/atoms/SvgIcon/InstagramSvg';
+import LinkedInSvg from '@/atoms/SvgIcon/LinkedInSvg';
 
 export default function Menu({ isDisplayed }) {
 
@@ -18,6 +22,13 @@ export default function Menu({ isDisplayed }) {
             style={styles}
             className="fixed overflow-hidden w-1/2 max-w-xs top-0 h-screen py-10">
             <PrimaryNavigation />
+
+            <nav className="menu__nav flex justify-around m-2">
+              <a title="My LinkedIn profile" aria-label="My LinkedIn profile" className="w-6" href="https://www.linkedin.com/in/barnspowell/" target="_blank" rel="noopener noreferrer"><LinkedInSvg /></a>
+              <a title="My Twitter feed" aria-label="My Twitter feed" className="w-8" href="https://twitter.com/BrnyPwll" target="_blank" rel="noopener noreferrer"><TwitterSvg /></a>
+              <a title="My Instagram profile" aria-label="My Instagram profile" className="w-8" href="https://www.instagram.com/barneyp/" target="_blank" rel="noopener noreferrer"><InstagramSvg /></a>
+              <a title="My IMdB entry" aria-label="My IMdB entry" className="w-8" href="https://www.imdb.com/name/nm6402749/" target="_blank" rel="noopener noreferrer"><ImdbSvg /></a>
+            </nav>
         </div>
     )
 }
