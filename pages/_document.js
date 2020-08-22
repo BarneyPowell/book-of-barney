e25011ab0c5f2c3852749735d1733e7ec631075d
Barney Powell <9741234+BarneyPowell@users.noreply.github.com>
2020-08-22 14:04:08 +0100

    Add Google Tag Manager script (#11)
    Co-authored-by: Barney Powell <barneyp@gmail.com>

--- pages/_document.js
+++ pages/_document.js
@@ -1,5 +1,5 @@
 import Document, { Head, Main, NextScript, Html } from "next/document";
-
+import GTag from '@/molecules/GTag';
 import getSiteMetadata from "../utils/helpers/getSiteMetadata";
 
 export default class MyDocument extends Document {
@@ -10,6 +10,7 @@ export default class MyDocument extends Document {
     return (
       <Html lang={siteMetadata.language}>
         <Head>
+          <GTag gaTrackingId='UA-3453155-3' />
         </Head>
         <body className="font-body">
           <Main />
